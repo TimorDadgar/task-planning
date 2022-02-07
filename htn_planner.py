d9527224d1ed219ee2276bcf9f5e725af797b740
Lukas E <lukeih181@studentmail.oru.se>
2022-02-07 11:10:43 +0100

    add htn_planner

---
+++ htn_planner.py
@@ -0,0 +1,65 @@
+from algorithm import *
+
+class State():
+    def __init__(self, name):
+        self.__name__ = name
+        self.pos = {}
+        self.carrying = None
+        self.pictureTaken = {}
+
+#Operators
+class goto(state, p):
+    state.pos['r'] = p
+    return state
+
+class pickup(state,i):
+    state.carrying = i
+    return state
+
+class drop(state):
+    state.carrying = None
+    return state
+
+class capture(state, pos):
+    state.pictureTaken |= {pos}
+    return state
+
+#Methods
+class move_robot(state,l1,l2):
+    if(state.pos['r'] == l1):
+        return [('goto', l2)]
+    else:
+        return False
+
+class pickup_item(state,loc,i):
+    if(state.carrying == None && state.pos['r'] == loc):
+        return [('pickup'), i]
+    else:
+        return False
+
+class drop_item(state,loc,i):
+    if(state.carrying == i && state.pos['r'] == loc):
+        return [('drop'), i]
+    else:
+        return False
+
+class take_picture(state,loc,c):
+    if(state.pos['r'] == loc):
+        return [('capture'), c]
+    else:
+        return False
+
+class search(problem, heuristic):
+    plan = best_first_graph_search(problem, heuristic)
+    return plan
+
+
+
+
+
+
+
+#declaration of problem and heuristic
+problem = ....
+heuristic = ....
+plan = search(problem,heuristic)
