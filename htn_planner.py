e1fb82384da7d37b282361807569d6ff6fba1d39
Lukas E <lukeih181@studentmail.oru.se>
2022-02-08 09:32:53 +0100

    update htn_planner

--- htn_planner.py
+++ htn_planner.py
@@ -1,50 +1,49 @@
 from algorithm import *
 
 class State():
-    def __init__(self, name):
-        self.__name__ = name
+    def __init__(self):
         self.pos = {}
         self.carrying = None
 
 #Operators
-class goto(state, p):
+def goto(state, p):
     state.pos['r'] = p
     return state
 
-class pickup(state,i):
+def pickup(state,i):
     state.carrying = i
     return state
 
-class drop(state):
+def drop(state):
     state.carrying = None
     return state
 
 #Methods
-class move_robot(state,l1,l2):
+def move_robot(state,l1,l2):
     if(state.pos['r'] == l1):
         return [('goto', l2)]
     else:
         return False
 
-class pickup_item(state,loc,i):
+def pickup_item(state,loc,i):
     if(state.carrying == None and state.pos['r'] == loc):
         return [('pickup'), i]
     else:
         return False
 
-class drop_item(state,loc,i):
+def drop_item(state,loc,i):
     if(state.carrying == i and state.pos['r'] == loc):
         return [('drop'), i]
     else:
         return False
 
-class take_picture(state,loc,c):
+def take_picture(state,loc,c):
     if(state.pos['r'] == loc):
         return [('capture'), c]
     else:
         return False
 
-class search(problem, heuristic):
+def search(problem, heuristic):
     plan = best_first_graph_search(problem, heuristic)
     return plan
 
@@ -55,6 +54,7 @@ class search(problem, heuristic):
 
 
 #declaration of problem and heuristic
-problem = #intial state, goal state, actions
-heuristic = #calculation of the heuristic function
-plan = search(problem,heuristic)
+#problem = #intial state, goal state, actions
+#heuristic = #calculation of the heuristic function
+#plan = search(problem,heuristic)
+#plan = {........}
